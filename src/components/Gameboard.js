d49cf22b78831b34c06cbc2d7f7a1220d7b7ec41
MissLiner <miss.liner@yahoo.com>
2022-03-07 14:04:54 -0500

    Add all flag images

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -2,12 +2,40 @@ import React from "react";
 import uniqid from "uniqid";
 import AntiguaAndBarbuda from "../resources/AntiguaAndBarbuda.png";
 import Argentina from "../resources/Argentina.png";
+import Guadaloupe from "../resources/Guadaloupe.png";
+import Japan from "../resources/Japan.png";
+import Kazakhstan from "../resources/Kazakhstan.png";
+import Kiribati from "../resources/Kiribati.png";
+import Kyrgyzstan from "../resources/Kyrgyzstan.png";
+import Macedonia from "../resources/Macedonia.png";
+import Malawi from "../resources/Malawi.png";
+import Malaysia from "../resources/Malaysia.png";
+import Namibia from "../resources/Namibia.png";
+import Nepal from "../resources/Nepal.png";
+import Phillipines from "../resources/Phillipines.png";
+import Rwanda from "../resources/Rwanda.png";
+import Taiwan from "../resources/Taiwan.png";
+import Uruguay from "../resources/Uruguay.png";
 import "../App.css"
 
 const Gameboard = (props) => {
   const flagsInfo = [
     {image: AntiguaAndBarbuda, text: "Antigua & Barbuda"},
     {image: Argentina, text: "Argentina"},
+    {image: Guadaloupe, text: "Guadaloupe"},
+    {image: Japan, text: "Japan"},
+    {image: Kazakhstan, text: "Kazakhstan"},
+    {image: Kiribati, text: "Kiribati"},
+    {image: Kyrgyzstan, text: "Kyrgyzstan"},
+    {image: Macedonia, text: "Macedonia"},
+    {image: Malawi, text: "Malawi"},
+    {image: Malaysia, text: "Malaysia"},
+    {image: Namibia, text: "Namibia"},
+    {image: Nepal, text: "Nepal"},
+    {image: Phillipines, text: "Phillipines"},
+    {image: Rwanda, text: "Rwanda"},
+    {image: Taiwan, text: "Taiwan"},
+    {image: Uruguay, text: "Uruguay"},
   ];
   const renderedFlags =
     flagsInfo.map((flag) => {
@@ -19,8 +47,7 @@ const Gameboard = (props) => {
         </div>
       )
     }
-
-    )
+  )
 
   return(
     <div className="cardsBox">
