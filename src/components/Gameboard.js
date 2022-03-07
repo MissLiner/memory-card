15e23f59584d89af87f09676bd7b7abde23e2dcc
MissLiner <miss.liner@yahoo.com>
2022-03-07 14:51:20 -0500

    Move flag images and array to separate component

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -1,42 +1,11 @@
 import React from "react";
 import uniqid from "uniqid";
-import AntiguaAndBarbuda from "../resources/AntiguaAndBarbuda.png";
-import Argentina from "../resources/Argentina.png";
-import Guadeloupe from "../resources/Guadeloupe.png";
-import Japan from "../resources/Japan.png";
-import Kazakhstan from "../resources/Kazakhstan.png";
-import Kiribati from "../resources/Kiribati.png";
-import Kyrgyzstan from "../resources/Kyrgyzstan.png";
-import Macedonia from "../resources/Macedonia.png";
-import Malawi from "../resources/Malawi.png";
-import Malaysia from "../resources/Malaysia.png";
-import Namibia from "../resources/Namibia.png";
-import Nepal from "../resources/Nepal.png";
-import Phillipines from "../resources/Phillipines.png";
-import Rwanda from "../resources/Rwanda.png";
-import Taiwan from "../resources/Taiwan.png";
-import Uruguay from "../resources/Uruguay.png";
+import { flagArray } from "./FlagArray";
 import "../App.css"
 
 const Gameboard = (props) => {
-  const flagsInfo = [
-    {image: AntiguaAndBarbuda, text: "Antigua & Barbuda"},
-    {image: Argentina, text: "Argentina"},
-    {image: Guadeloupe, text: "Guadeloupe"},
-    {image: Japan, text: "Japan"},
-    {image: Kazakhstan, text: "Kazakhstan"},
-    {image: Kiribati, text: "Kiribati"},
-    {image: Kyrgyzstan, text: "Kyrgyzstan"},
-    {image: Macedonia, text: "Macedonia"},
-    {image: Malawi, text: "Malawi"},
-    {image: Malaysia, text: "Malaysia"},
-    {image: Namibia, text: "Namibia"},
-    {image: Nepal, text: "Nepal"},
-    {image: Phillipines, text: "Phillipines"},
-    {image: Rwanda, text: "Rwanda"},
-    {image: Taiwan, text: "Taiwan"},
-    {image: Uruguay, text: "Uruguay"},
-  ];
+  const { clickFunc } = props;
+
   const shuffleArray = (array) => {
     const shuffledArray = array;
     for (let i = array.length - 1; i > 0; i--) {
@@ -49,7 +18,7 @@ const Gameboard = (props) => {
   }
 
   const renderFlags = () => {
-    const shuffledFlags = shuffleArray(flagsInfo);
+    const shuffledFlags = shuffleArray(flagArray);
     return(
       shuffledFlags.map((flag) => {
         const keyID = uniqid();
@@ -70,7 +39,6 @@ const Gameboard = (props) => {
     )
   }
     
-
   return(
     <div className="cardsBox">
       {renderFlags()}
---
+++ src/components/flagArray.js
@@ -0,0 +1,35 @@
+import AntiguaAndBarbuda from "../resources/AntiguaAndBarbuda.png";
+import Argentina from "../resources/Argentina.png";
+import Guadeloupe from "../resources/Guadeloupe.png";
+import Japan from "../resources/Japan.png";
+import Kazakhstan from "../resources/Kazakhstan.png";
+import Kiribati from "../resources/Kiribati.png";
+import Kyrgyzstan from "../resources/Kyrgyzstan.png";
+import Macedonia from "../resources/Macedonia.png";
+import Malawi from "../resources/Malawi.png";
+import Malaysia from "../resources/Malaysia.png";
+import Namibia from "../resources/Namibia.png";
+import Nepal from "../resources/Nepal.png";
+import Phillipines from "../resources/Phillipines.png";
+import Rwanda from "../resources/Rwanda.png";
+import Taiwan from "../resources/Taiwan.png";
+import Uruguay from "../resources/Uruguay.png";
+
+export const flagArray = [
+  {image: AntiguaAndBarbuda, text: "Antigua & Barbuda"},
+  {image: Argentina, text: "Argentina"},
+  {image: Guadeloupe, text: "Guadeloupe"},
+  {image: Japan, text: "Japan"},
+  {image: Kazakhstan, text: "Kazakhstan"},
+  {image: Kiribati, text: "Kiribati"},
+  {image: Kyrgyzstan, text: "Kyrgyzstan"},
+  {image: Macedonia, text: "Macedonia"},
+  {image: Malawi, text: "Malawi"},
+  {image: Malaysia, text: "Malaysia"},
+  {image: Namibia, text: "Namibia"},
+  {image: Nepal, text: "Nepal"},
+  {image: Phillipines, text: "Phillipines"},
+  {image: Rwanda, text: "Rwanda"},
+  {image: Taiwan, text: "Taiwan"},
+  {image: Uruguay, text: "Uruguay"},
+];
