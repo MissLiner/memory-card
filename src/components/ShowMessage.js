6732cc03e82531092e0667a46983f3c8491acb56
MissLiner <miss.liner@yahoo.com>
2022-03-08 00:26:53 -0500

    Edit win message

--- src/components/ShowMessage.js
+++ src/components/ShowMessage.js
@@ -4,7 +4,7 @@ import '../App.css';
 const ShowMessage = (props) => {
   const { message, clickFunc } = props;
     const messages = {
-    win1: "You picked all the cards! Starting with a new deck, can you do it again?",
+    win1: "You made it through the whole deck! Starting with a new deck, can you do it again?",
     win2: "You did it again! Can you run through the deck a THIRD time?!?",
     win3: "You're either a memory genius, or you're cheating! Either way, you're starting back at 0",
     startGame: "Welcome to the Sun Flag Memory Card game! You get one point for each card you click on, as long as you don't click the same card twice. Happy clicking!!",
