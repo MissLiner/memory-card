1af3d2cdb1a465bdbafc90e036cd446d85a898ee
MissLiner <miss.liner@yahoo.com>
2022-03-08 00:38:45 -0500

    Make messageBox smaller, add title

--- src/components/ShowMessage.js
+++ src/components/ShowMessage.js
@@ -4,16 +4,17 @@ import '../App.css';
 const ShowMessage = (props) => {
   const { message, clickFunc } = props;
     const messages = {
-    win1: "You made it through the whole deck! Starting with a new deck, can you do it again?",
-    win2: "You did it again! Can you run through the deck a THIRD time?!?",
-    win3: "You're either a memory genius, or you're cheating! Either way, you're starting back at 0",
-    startGame: "Welcome to the Sun Flag Memory Card game! You get one point for each card you click on, as long as you don't click the same card twice. Happy clicking!!",
-    none: "",
+    win1: ["WIN!", "You made it through the whole deck! Starting with a new deck, can you do it again?"],
+    win2: ["WIN 2!", "You did it again! Can you run through the deck a THIRD time?!?"],
+    win3: ["WIN 3!", "You're either a memory genius, or you're cheating! Either way, you're starting back at 0"],
+    startGame: ["Welcome!",  "How to play the Sun Flag Memory Card game: Click on a card, get a point, just don't click the same card twice. Happy clicking!"],
+    none: ["", ""]
   }
   if(message !== "none") {
     return(
       <div className="messageBox">
-        <div className='messageText'>{messages[message]}</div>
+        <div className="messageTitle">{messages[message][0]}</div>
+        <div className='messageText'>{messages[message][1]}</div>
         <button className="messageBtn" onClick={clickFunc}>Let's Play!</button>
       </div>
     )
