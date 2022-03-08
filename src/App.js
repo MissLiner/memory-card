2ae27a2e3dd9403a721e9bed07b8bbdf7c9090fd
MissLiner <miss.liner@yahoo.com>
2022-03-07 20:03:18 -0500

    Disable winAlert and fix bug in playCard

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import './App.css';
 import { flagArray } from './components/flagArray';
 import Gameboard from './components/Gameboard';
 import Scoreboard from './components/Scoreboard';
-import WinAlert from './components/WinAlert';
 
 function App() {
   const [score, setScore] = useState(0);
   const [deck, setDeck] = useState([]);
 
-  useEffect(() => {
-    fillDeck();
-  }, []);
+
 
   const emptyDeck = () => {
     const arrLength = deck.length;
@@ -21,9 +18,12 @@ function App() {
     const newCards = flagArray.map(flag => flag.name)
     setDeck(deck.concat(newCards));
   }
+  useEffect(() => {
+    fillDeck();
+  }, []);
 
   const playCard = (e) => {
-    const cardClicked = e.target.id;
+    const cardClicked = e.target.dataset.name;
     if(deck.includes(cardClicked)) {
       setDeck(deck.filter(card => card !== cardClicked));
       setScore(score + 1);
@@ -39,7 +39,6 @@ function App() {
       <h1>Memory Card</h1>
       <Scoreboard score={score} />
       <Gameboard clickFunc={playCard} score={score} />
-      <WinAlert score={score} />
     </div>
   );
 }
