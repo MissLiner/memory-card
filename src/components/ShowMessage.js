5db56619a4d0bb154e30fa7da7b46e5e49fb1a8d
MissLiner <miss.liner@yahoo.com>
2022-07-06 15:53:36 -0400

    Optimize for mobile

--- src/components/ShowMessage.js
+++ src/components/ShowMessage.js
@@ -7,7 +7,7 @@ const ShowMessage = (props) => {
     win1: ["WIN!", "You made it through the whole deck! Starting with a new deck, can you do it again?"],
     win2: ["WIN 2!", "You did it again! Can you run through the deck a THIRD time?!?"],
     win3: ["WIN 3!", "You're either a memory genius, or you're cheating! Either way, you're starting back at 0"],
-    startGame: ["Welcome!",  "How to play the Sun Flag Memory Card game: click on a card, get a point, just don't click the same card twice. Happy clicking!"],
+    startGame: ["WELCOME!",  "Pick a card and get a point, just don't pick the same card twice. Happy clicking!"],
     none: ["", ""]
   }
   if(message !== "none") {
@@ -15,7 +15,7 @@ const ShowMessage = (props) => {
       <div className="messageBox">
         <div className="messageTitle">{messages[message][0]}</div>
         <div className='messageText'>{messages[message][1]}</div>
-        <button className="messageBtn" onClick={clickFunc}>Let's Play!</button>
+        <button className="messageBtn" onClick={clickFunc}>LET'S PLAY</button>
       </div>
     )
   } else {
